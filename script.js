96d91327e15e08e1b016994d7ca88f8c463397cd
MohamedHamouch <med10hamouch@gmail.com>
2024-11-07 01:39:40 +0100

    changes

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function displayCard(cardToDisplay) {
             setTimeout(() => {
                 if (firstCardColor === secondCardColor) {
                     alert("You found twins");
-                    checkWin(cards);ml
+                    checkWin(cards);    
                     wonCards = wonCards.concat(clickedCards)
                 } else {
                     alert("Try again");
@@ -51,3 +51,62 @@ function checkWin(allCards){
     }
     if(wonStatus) alert("Game Cleared!! You Won !!!")
 }
+
+
+
+
+// const cards = document.querySelectorAll(".flip-card");
+// let clickedCards = [];
+// let wonCards = [];
+
+
+// for (let card of cards) {
+//     console.log(card);
+//     card.addEventListener("click", () => displayCard(card));
+// }
+
+// function displayCard(cardToDisplay) {
+//     const clickedBefor = clickedCards.includes(cardToDisplay);
+//     const wonBefor = wonCards.includes(cardToDisplay);
+//     if (clickedCards.length < 2 && !clickedBefor && !wonBefor) {
+//         cardToDisplay.classList.toggle("flip");
+//         clickedCards.push(cardToDisplay);
+//         if (clickedCards.length === 2) {
+
+//             // const firstCardColor = getComputedStyle(clickedCards[0]).backgroundColor;
+//             // const secondCardColor = getComputedStyle(clickedCards[1]).backgroundColor;
+//             const firstID = clickedCards[0].id
+//             const secondID = clickedCards[1].id
+
+//             setTimeout(() => {
+//                 if (firstID === secondID) {
+//                     alert("You found twins");
+//                     checkWin(cards);    
+//                     wonCards = wonCards.concat(clickedCards)
+//                 } else {
+//                     alert("Try again");
+//                     for (let clickedCard of clickedCards) {
+//                         clickedCard.classList.toggle("flip");
+//                     }
+//                 }
+//                 clickedCards = [];
+//             }, 100);
+
+//         }
+//     } else if (clickedBefor) {
+//         alert("Card already clicked")
+//     }else if (wonBefor){
+//         alert("this card already found it's twin")
+//     }
+    
+// }
+// function checkWin(allCards){
+//     wonStatus = true;
+//     for(let card of allCards){
+//         if (card.classList.contains("flip")){
+//             wonStatus = false;
+//             break;
+//         }
+//     }
+//     if(wonStatus) alert("Game Cleared!! You Won !!!")
+// }
